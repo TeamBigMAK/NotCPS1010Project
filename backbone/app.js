57e227b6923a1693870cb41687d5e94f8df574c6
Aidan Cauchi <aidan.s.cauchi.17@um.edu.mt>
2018-03-07 20:37:01 +0100

    Server connecting to database and updating it. Also includes pooling.

--- backbone/app.js
+++ backbone/app.js
@@ -1,46 +1,39 @@
-var express = require('express'),
-    path = require('path'),
-    bodyParser = require('body-parser'),
-    cons = require('consolidate'),
-    dust = require('dustjs-helpers'),
-    pg = require('pg'),
-    app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+var pg = require("pg");
+var app = express();
 
-// Connect to DB STRING
-var connectionString = "postgres://projBB:123456@localhost:5432/poldb";
-
-// Assign dust engine to dust file
-app.engine('dust', cons.dust);
-
-// Set dust as default extension
-app.set('view engine', 'dust');
-app.set('views', __dirname + "/views");
-
-// Set public folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-
-// PG Connect
-app.get('/', function(req, res){
-    pg.connect(connectionString, function(err, client, done){
-        if(err) {
-            return console.error('error fetching client from pool', err);
-        }
-        client.query('SELECT * FROM test', function(err, result){
-            if(err){
-                return console.error('error running query', err);
-            }
-            res.render('index', {test: result.rows});
-            done();
-        });
-    });
+const pool = new pg.Pool({
+user: 'testUser',
+host: 'localhost',
+database: 'testDB',
+password: 'passw',
+port: 5432,
 });
 
 
-// Server
-app.listen(3030, function(){
-    console.log('Server started on port 3030');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine","ejs");
+
+app.get("/questions",function(req,res){
+  pool.query('SELECT * FROM test.questions',
+function(err,result){
+  if(err)console.log(err);
+  var questions = result.rows;
+  res.render("ques",{questions: questions});
+    })
+});
+
+app.post("/addQues",function(req,res){
+  var newQuestion = req.body.newQues;
+  pool.query('INSERT INTO test.questions(qname) VALUES ($1)',[newQuestion],
+  function(err,result){
+  if(err)console.log(err);
+  res.redirect("/questions");
+  })
+});
+
+app.listen(3000,process.env.IP,function(){
+  console.log("Server is listening on port 3000!");
 });
